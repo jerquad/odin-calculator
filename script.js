c6162781c7f62d3157030cd42fe5d73e8d41e3c7
Jimmy <jimmyquadros@hotmail.com>
2022-08-31 11:47:25 -0400

    add basic functions, add unit testing

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = (function (a, b) {
+    return a + b;
+});
+
+const subtract = (function (a, b) {
+    return a - b;
+});
+
+const multiply = (function(a, b) {
+    return a * b;
+});
+
+const divide = (function(a, b) {
+    if (b == 0) { return 'err'; }
+    return a / b;
+});
+
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+};
---
+++ script.test.js
@@ -0,0 +1,50 @@
+const script = require('./script.js');
+
+describe('add', () => {
+    test('adds 0 and 0', () => {
+        expect(script.add(0,0)).toBe(0);
+    });
+
+    test('adds positive numbers', () => {
+		expect(script.add(2,6)).toBe(8);
+	});
+});
+
+describe('subtract', () => {
+	test('subtracts numbers', () => {
+		expect(script.subtract(10,4)).toBe(6);
+	});
+});
+
+describe('multiply', () => {
+    test('multiply positive numbers', () => {
+        expect(script.multiply(3, 4)).toBe(12);
+    });
+
+    test('multiply two negatives', () => {
+        expect(script.multiply(-4, -5)).toBe(20);
+    });
+
+    test('multiply postive and negative', () => {
+        expect(script.multiply(7, -2)).toBe(-14);
+    });
+
+    test('multiply by zero', () => {
+        expect(script.multiply(5, 0)).toBe(0);
+    });
+});
+
+describe('divide', () => {
+    test('divide positives', () => {
+        expect(script.divide(10, 2)).toBe(5);
+    });
+    test('divide negatives', () => {
+        expect(script.divide(-6, -3)).toBe(2);
+    });
+    test('divide zero by value', () => {
+        expect(script.divide(0, 5)).toBe(0);
+    });
+    test('division by 0', () => {
+        expect(script.divide(8, 0)).toBe('err');
+    });
+});
